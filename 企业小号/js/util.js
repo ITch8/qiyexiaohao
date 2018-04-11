382f86ca50e77a03b4e08864a403f722e3c9afaa
ITch8 <lichuang516@126.com>
2018-04-11 22:05:05 +0800

    布置底部菜单，新增联系人h5p功能
    布置底部菜单，新增联系人h5p功能

--- 企业小号/js/util.js
+++ 企业小号/js/util.js
@@ -2,7 +2,7 @@ var util = {
 	options: {
 		ACTIVE_COLOR: "#007aff",
 		NORMAL_COLOR: "#000",
-		subpages: ["html/tab-webview-subpage-chat.html", "html/contact_mint.html"]
+		subpages: ["html/tab-webview-subpage-chat.html", "html/contact_mint.html", "html/my.html"]
 	},
 	/**
 	 *  简单封装了绘制原生view控件的方法
